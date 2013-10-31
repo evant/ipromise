23502f3588329a6000e1ac0fdb530decf70336bf
Evan Tatarka <evan.tatarka@willowtreeapps.com>
2013-10-31 18:15:01 -0400

    starting fixing javadoc comments

--- src/main/java/me/tatarka/ipromise/Deferred.java
+++ src/main/java/me/tatarka/ipromise/Deferred.java
@@ -4,18 +4,20 @@ package me.tatarka.ipromise;
  * A {@code Deferred} is the producer end of a {@link Promise}. An asynchronous method creates a {@code Deferred} and
  * returns {@link Deferred#promise()}, then calls {@link Deferred#resolve(Object)} or {@link Deferred#reject(Exception)}
  * at a later time.
- * <pre>{@code
- *    public Promise<Result, Error> async() {
- *       final Deferred<Result, Error> deferred = new Deferred<Result, Error>();
- *       doAsync(new Callback() {
- *          @literal@Override
- *          public void onResult(Result result) {
- *             deferred.deliver(result);
- *          }
- *       }
- *       return deferred.promise();
- *    }
- * }</pre>
+ * <pre>
+ * <code>
+ * public Promise{@code <Result, Error>} async() {
+ *     final Deferred{@code <Result, Error>} deferred = new Deferred{@code <Result, Error>}();
+ *         doAsync(new Callback() {
+ *             {@literal @}Override
+ *             public void onResult(Result result) {
+ *                 deferred.deliver(result);
+ *             }
+ *         });
+ *     return deferred.promise();
+ * }
+ * </code>
+ * </pre>
  *
  * @param <T> the type of a successful result
  * @param <E> the type of an error result
